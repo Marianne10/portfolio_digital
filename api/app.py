c959199b2dab0874a576b97747f89740f71c9d38
Marianne10 <valerionunesm@gmail.com>
2024-06-12 10:32:15 -0300

    alteracao

---
+++ api/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template # type: ignore
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    return render_template('index.html')
+   
+@app.route('/sobreMim')
+def sobreMim():
+    return render_template('sobreMim.html')
+ 
+@app.route('/projetos')
+def projetos():
+    return render_template('projetos.html')
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
