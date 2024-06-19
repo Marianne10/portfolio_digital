0260cf42966fff2fb791570b32fd43d4b6717a35
Marianne10 <valerionunesm@gmail.com>
2024-06-19 00:20:20 -0300

    Implantação do css

--- api/app.py
+++ api/app.py
@@ -1,18 +1,35 @@
-from flask import Flask, render_template # type: ignore
+from flask import Flask, render_template 
  
-app = Flask(__name__)
+app = Flask (__name__)
  
 @app.route('/')
+def base():
+    return render_template('base.html')
+
+@app.route('/index')
 def index():
     return render_template('index.html')
-   
-@app.route('/sobreMim')
-def sobreMim():
-    return render_template('sobreMim.html')
+
+@app.route('/certificacoes')
+def certificacoes():
+    return render_template('certificacoes.html')
  
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html')
+
 @app.route('/projetos')
 def projetos():
     return render_template('projetos.html')
+
+@app.route('/contatos')
+def projetos():
+    return render_template('contatos.html')
+
+@app.route('/sobremim')
+def projetos():
+    return render_template('sobremim.html')
+ 
  
 if __name__ == '__main__':
     app.run(debug=True)
